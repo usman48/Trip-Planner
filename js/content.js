e10ec4beb4a6386fb4e6ad691197400c36205f3b
usman48 <usmanamjad076@icloud.com>
2022-10-15 02:44:12 +0200

    selection and error fixed

--- js/content.js
+++ js/content.js
@@ -21,3 +21,72 @@ var ScrapeData = {};
     }
     console.log(ScrapeData);
   }
+  document.addEventListener('mouseup', (event) => {
+    event.preventDefault();
+    // if (window.getSelection) {
+    //   if (window.getSelection().empty) {  // Chrome
+    //         window.getSelection().empty();
+    //               } else if (window.getSelection().removeAllRanges) {  // Firefox
+    //                   window.getSelection().removeAllRanges();
+    //               }
+    //           } else if (document.selection) {  // IE?
+    //               document.selection.empty();
+    //           }else{
+                (document.querySelector("#ttdiv")) ? document.querySelector("#ttdiv").remove() : '';
+                let Selection = window.getSelection().toString();
+                (Selection.length > 5) ? tooltip() : '';
+              // }
+
+  });
+//   document.addEventListener('mousedown', (event) => {
+//     event.preventDefault();
+//     setTimeout(() => {
+//         if (window.getSelection) {
+//             if (window.getSelection().empty) {  // Chrome
+//                 window.getSelection().empty();
+//             } else if (window.getSelection().removeAllRanges) {  // Firefox
+//                 window.getSelection().removeAllRanges();
+//             }
+//         } else if (document.selection) {  // IE?
+//             document.selection.empty();
+//         }
+//         (document.querySelector("#ttdiv")) ? document.querySelector("#ttdiv").remove() : '';
+//     }, 100)
+// })
+
+  var x = null;
+  var y = null;
+  
+  document.addEventListener('mousemove', onMouseUpdate, false);
+  document.addEventListener('mouseenter', onMouseUpdate, false);
+  
+  function onMouseUpdate(e) {
+    x = e.pageX;
+    y = e.pageY + 10;
+  }
+  function getMouseX() {
+    return x;
+  }
+  
+  function getMouseY() {
+    return y;
+  }
+  function tooltip() {
+    (document.querySelector("#ttdiv")) ?.remove();
+    var tooltip = document.createElement("span");
+    tooltip.id = 'ttdiv'
+    tooltip.style.position = 'absolute';
+    tooltip.style.border = '0px solid';
+    tooltip.style.padding = '4px';
+    tooltip.style.zIndex = '999999';
+    tooltip.style.background = 'rgba(17, 24, 39, 0.6)';
+    tooltip.style.borderRadius = '6px';
+    tooltip.style.color = '#FFFFFF'
+    tooltip.style.top = (getMouseY()) + 'px';
+    tooltip.style.left = getMouseX() + 'px';
+    tooltip.innerHTML = 'Selected text is too short for our AI 😅';
+    document.body.appendChild(tooltip);
+    return;
+  }
+
+ 
--- js/popup.js
+++ js/popup.js
@@ -8,48 +8,50 @@ document.addEventListener("DOMContentLoaded", function() {
         });
       });
   });
+  if(document.getElementById('save-btn')){
+    document.getElementById('save-btn').addEventListener('click', function () {
+        let url = document.getElementById('text-input').value;
+        if (url != '') {
+            let capturedId = url.match(/\/d\/(.+)\//);
+            console.log(capturedId[1]);
+            chrome.storage.local.set({sheetId: capturedId[1]});
+            chrome.storage.local.set({sheetUrl: url});
+            const SHEET_ID = chrome.storage.local.get(['accessToken']);
+            SHEET_ID.then((res) => {
+                console.log(res.accessToken);
+                const base = `https://sheets.googleapis.com/v4/spreadsheets/${capturedId[1]}`;
+                fetch(base,{
+                    method: "GET",
+                    headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${res.accessToken}`
+                    }}).then(res => res.json()).then(rep => {
+                            (document.getElementById("sheet_name").parentElement.style.display == 'none') ? document.getElementById("sheet_name").parentElement.style.display = 'block' : ''
+                            document.getElementById("sheet_name").innerText = rep.properties.title;
+                            // document.querySelector('select').innerHTML = "";
+                            // rep.sheets.forEach(titles);
+                            // function titles(item,index){
+                            //     console.log(item.properties.title);
+                            //     const option = document.createElement("OPTION");
+                            //     const name = document.createTextNode(item.properties.title);
+                            //     option.value = item.properties.title;
+                            //     option.appendChild(name);
+                            //     document.querySelector('select').appendChild(option);
+                            //     }
+                                // const sheetName = document.getElementById('select').value;
+                                // browser.storage.local.set({'sheetName': sheetName});
+                                // document.getElementById('select').addEventListener('change', function(e) {
+                                //      browser.storage.local.set({'sheetName': e.target.value});
+                                //             });
+                        })
+        
+        
+        
+            })
+        } 
+    });
+  }
 
-document.getElementById('save-btn').addEventListener('click', function () {
-    let url = document.getElementById('text-input').value;
-    if (url != '') {
-        let capturedId = url.match(/\/d\/(.+)\//);
-        console.log(capturedId[1]);
-        chrome.storage.local.set({sheetId: capturedId[1]});
-        chrome.storage.local.set({sheetUrl: url});
-        const SHEET_ID = chrome.storage.local.get(['accessToken']);
-        SHEET_ID.then((res) => {
-            console.log(res.accessToken);
-            const base = `https://sheets.googleapis.com/v4/spreadsheets/${capturedId[1]}`;
-            fetch(base,{
-                method: "GET",
-                headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${res.accessToken}`
-                }}).then(res => res.json()).then(rep => {
-                        (document.getElementById("sheet_name").parentElement.style.display == 'none') ? document.getElementById("sheet_name").parentElement.style.display = 'block' : ''
-                        document.getElementById("sheet_name").innerText = rep.properties.title;
-                        // document.querySelector('select').innerHTML = "";
-                        // rep.sheets.forEach(titles);
-                        // function titles(item,index){
-                        //     console.log(item.properties.title);
-                        //     const option = document.createElement("OPTION");
-                        //     const name = document.createTextNode(item.properties.title);
-                        //     option.value = item.properties.title;
-                        //     option.appendChild(name);
-                        //     document.querySelector('select').appendChild(option);
-                        //     }
-                            // const sheetName = document.getElementById('select').value;
-                            // browser.storage.local.set({'sheetName': sheetName});
-                            // document.getElementById('select').addEventListener('change', function(e) {
-                            //      browser.storage.local.set({'sheetName': e.target.value});
-                            //             });
-                    })
-    
-    
-    
-        })
-    } 
-});
 
 
 
